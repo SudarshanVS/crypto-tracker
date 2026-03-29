58ba4b15c6f1e8151d864619730f785d23652308
vssudarshan <vssudarshan89@gmail.com>
2026-03-30 01:36:07 +0530

    build(app): Commit config files for frontend and backend.

---
+++ backend/config/settings.py
@@ -0,0 +1,13 @@
+import logging
+import os
+REDIS_URI = os.getenv("REDIS_URL", "redis://127.0.0.1:6379/0")
+REDIS_MAX_CONNECTIONS = 100
+
+STREAMS = os.getenv("STREAMS", "ethusdt@ticker/solusdt@ticker/btcusdt@ticker")
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOST", "http://localhost:3000,http://127.0.0.1:3000").split(",")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
