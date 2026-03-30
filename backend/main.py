6c9a1e4a58f621056442f2ff5a455713050c69a6
vssudarshan <vssudarshan89@gmail.com>
2026-03-31 02:04:01 +0530

    build(backend): Make updates to file so that they match the config found at https://github.com/vercel/vercel/tree/main/examples/fastapi

--- backend/main.py
+++ backend/main.py
@@ -72,4 +72,4 @@ async def price(crypto_symbols: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=5001)
